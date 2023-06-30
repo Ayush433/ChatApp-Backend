f81ca4873828d8e08e17f12cd2d6c2d25de183cb
Ayush Adhikari <adhikariayush19@gmail.comgit config --global user.nameAyush433>
2023-06-30 12:38:42 +0545

    socket implemented but having problem in receiving side yet to fixed this bug

--- app.js
+++ app.js
@@ -4,9 +4,48 @@ const dotenv = require("dotenv");
 const userRoutes = require("./Routes/userRoutes");
 const messageRoutes = require("./Routes/messageRoutes");
 const cors = require("cors");
+const io = require("socket.io")(4040, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
 dotenv.config();
 const port = process.env.PORT || 9000;
 const dbURI = process.env.DATABASE;
+
+//Socket.io
+let users = [];
+io.on("connection", (socket) => {
+  console.log("User-Connected", socket.id);
+  socket.on("addUser", (userId) => {
+    const isUserExist = users.find((user) => user.userId === userId);
+    if (!isUserExist) {
+      const user = { userId, socketId: socket.id };
+      users.push(user);
+      io.emit("getUser", users);
+    }
+  });
+
+  socket.on(
+    "sendMessages",
+    async ({ senderId, message, conversationId, receiverId }) => {
+      const receiver = users.find((user) => user.userId);
+      // const sender = users.find((user) => user.senderId);
+      if (receiver) {
+        io.to(receiver.socketId).to(sender.socketId).emit("getMessage", {
+          senderId,
+          message,
+          conversationId,
+        });
+      }
+    }
+  );
+  socket.on("disconnect", () => {
+    users = users.filter((user) => user.socketId !== socket.id);
+    io.emit("getUser", users);
+  });
+  //   io.emit("getUsers",socket.userId)
+});
 
 const app = express();
 app.use(cors());
