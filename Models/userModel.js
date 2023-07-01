8adb304f7582274f314b6b3b1c32009fd976cee4
Ayush Adhikari <adhikariayush19@gmail.comgit config --global user.nameAyush433>
2023-07-01 16:49:02 +0545

    chat app backedn task completed

--- Models/userModel.js
+++ Models/userModel.js
@@ -15,10 +15,6 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
     },
-    confirmPassword: {
-      type: String,
-      required: true,
-    },
   },
   { timestamps: true }
 );
